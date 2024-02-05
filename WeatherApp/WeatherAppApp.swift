5fb87280f325636c9a95eb5f12fe614d116894da
LouiseMustafaeva <112573505+LouiseMustafaeva@users.noreply.github.com>
2024-02-05 21:01:49 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Louise on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
